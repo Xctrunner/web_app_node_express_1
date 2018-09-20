3b87828cc555377c65d242a5fd448e84bcb23951
Walter Martin <wamartin@microsoft.com>
2018-09-20 09:18:42 -0400

    add linting

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 // can use 'tiny' for less info
 app.use(morgan('combined'));
@@ -16,10 +16,10 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/'))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/')));
 
 app.get('/', (req, res) => {
-    // res.send('Hello from my library app');
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+  // res.send('Hello from my library app');
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.listen(3002, () => {
-    debug(`listening on port ${chalk.green('3002')}`);
+  debug(`listening on port ${chalk.green('3002')}`);
 });
